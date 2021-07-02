5d7c5ae591d8082d76983bf73be66f7ba90400fa
3ª Via - 2022 <86832379+3avia@users.noreply.github.com>
2021-07-02 09:26:05 -0300

    New

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center" class="hm-100">
+    <v-col class="pa-6">
+      <a href="mailto:contato@3avia.org"><img src="/logo.svg" alt="" /></a>
+
+      <p class="text-center">Entre em contato pelo email contato@3avia.org</p>
+    </v-col>
+  </v-row>
+</template>
+
+<style lang="scss" scoped>
+.hm-100 {
+  min-height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 5;
+}
+img {
+  max-width: 45vw;
+  margin: 0px auto 30px;
+  display: block;
+}
+</style>
